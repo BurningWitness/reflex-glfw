6ed34539788deec23475b8ba7d94ae02bb369f79
BurningWitness <frozenwitness@gmail.com>
2021-11-27 16:48:54 +0700

    Moving defaults for callback overrides from Def to the class definition

--- src/Reflex/Host/GLFW/Window.hs
+++ src/Reflex/Host/GLFW/Window.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE RecursiveDo #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -62,44 +63,140 @@ data WindowE c t =
 --   the types you wish to extend.
 class Conversion c where
   type WindowPosE          c :: *
+  type WindowPosE          c = (Window, Int, Int)
+
   type WindowSizeE         c :: *
+  type WindowSizeE         c = (Window, Int, Int)
+
   type WindowCloseE        c :: *
+  type WindowCloseE        c =  Window
+
   type WindowRefreshE      c :: *
+  type WindowRefreshE      c =  Window
+
   type WindowFocusE        c :: *
+  type WindowFocusE        c = (Window, Bool)
+
   type WindowIconifyE      c :: *
+  type WindowIconifyE      c = (Window, Bool)
+
   type FramebufferSizeE    c :: *
+  type FramebufferSizeE    c = (Window, Int, Int)
 #if MIN_VERSION_GLFW_b(3,3,0)
   type WindowContentScaleE c :: *
+  type WindowContentScaleE c = (Window, Float, Float)
+
   type WindowMaximizeE     c :: *
+  type WindowMaximizeE     c = (Window, Bool)
 #endif
   type KeyE                c :: *
-  type CharE               c :: *
-  type CharModsE           c :: *
-  type MouseButtonE        c :: *
-  type CursorPosE          c :: *
-  type CursorEnterE        c :: *
-  type ScrollE             c :: *
-  type DropE               c :: *
+  type KeyE                c = (Window, Key, Int, KeyState, ModifierKeys)
 
-  windowPosConv          :: Proxy c -> WindowPosE          Def -> IO (WindowPosE          c)
-  windowSizeConv         :: Proxy c -> WindowSizeE         Def -> IO (WindowSizeE         c)
-  windowCloseConv        :: Proxy c -> WindowCloseE        Def -> IO (WindowCloseE        c)
-  windowRefreshConv      :: Proxy c -> WindowRefreshE      Def -> IO (WindowRefreshE      c)
-  windowFocusConv        :: Proxy c -> WindowFocusE        Def -> IO (WindowFocusE        c)
-  windowIconifyConv      :: Proxy c -> WindowIconifyE      Def -> IO (WindowIconifyE      c)
-  framebufferSizeConv    :: Proxy c -> FramebufferSizeE    Def -> IO (FramebufferSizeE    c)
+  type CharE               c :: *
+  type CharE               c = (Window, Char)
+
+  type CharModsE           c :: *
+  type CharModsE           c = (Window, Char, ModifierKeys)
+
+  type MouseButtonE        c :: *
+  type MouseButtonE        c = (Window, MouseButton, MouseButtonState, ModifierKeys)
+
+  type CursorPosE          c :: *
+  type CursorPosE          c = (Window, Double, Double)
+
+  type CursorEnterE        c :: *
+  type CursorEnterE        c = (Window, CursorState)
+
+  type ScrollE             c :: *
+  type ScrollE             c = (Window, Double, Double)
+
+  type DropE               c :: *
+  type DropE               c = (Window, [FilePath])
+
+
+  windowPosConv :: Proxy c -> (Window, Int, Int) -> IO (WindowPosE c)
+  default windowPosConv
+             :: (Window, Int, Int) ~ WindowPosE c => Proxy c -> (Window, Int, Int) -> IO (WindowPosE c)
+  windowPosConv          _ = return
+
+  windowSizeConv :: Proxy c -> (Window, Int, Int) -> IO (WindowSizeE c)
+  default windowSizeConv
+            :: (Window, Int, Int) ~ WindowSizeE c => Proxy c -> (Window, Int, Int) -> IO (WindowSizeE c)
+  windowSizeConv         _ = return
+
+  windowCloseConv :: Proxy c -> Window -> IO (WindowCloseE c)
+  default windowCloseConv :: Window ~ WindowCloseE c => Proxy c -> Window -> IO (WindowCloseE c)
+  windowCloseConv        _ = return
+
+  windowRefreshConv :: Proxy c -> Window -> IO (WindowRefreshE c)
+  default windowRefreshConv :: Window ~ WindowRefreshE c => Proxy c -> Window -> IO (WindowRefreshE c)
+  windowRefreshConv      _ = return
+
+  windowFocusConv :: Proxy c -> (Window, Bool) -> IO (WindowFocusE c)
+  default windowFocusConv
+            :: (Window, Bool) ~ WindowFocusE c => Proxy c -> (Window, Bool) -> IO (WindowFocusE c)
+  windowFocusConv        _ = return
+
+  windowIconifyConv :: Proxy c -> (Window, Bool) -> IO (WindowIconifyE c)
+  default windowIconifyConv
+            :: (Window, Bool) ~ WindowIconifyE c => Proxy c -> (Window, Bool) -> IO (WindowIconifyE c)
+  windowIconifyConv      _ = return
+
+  framebufferSizeConv :: Proxy c -> (Window, Int, Int) -> IO (FramebufferSizeE c)
+  default framebufferSizeConv
+            :: (Window, Int, Int) ~ FramebufferSizeE c => Proxy c -> (Window, Int, Int) -> IO (FramebufferSizeE c)
+  framebufferSizeConv    _ = return
 #if MIN_VERSION_GLFW_b(3,3,0)
-  windowContentScaleConv :: Proxy c -> WindowContentScaleE Def -> IO (WindowContentScaleE c)
-  windowMaximizeConv     :: Proxy c -> WindowMaximizeE     Def -> IO (WindowMaximizeE     c)
+  windowContentScaleConv :: Proxy c -> (Window, Float, Float) -> IO (WindowContentScaleE c)
+  default windowContentScaleConv
+            :: (Window, Float, Float) ~ WindowContentScaleE c
+            => Proxy c -> (Window, Float, Float) -> IO (WindowContentScaleE c)
+  windowContentScaleConv _ = return
+
+  windowMaximizeConv :: Proxy c -> (Window, Bool) -> IO (WindowMaximizeE c)
+  default windowMaximizeConv
+            :: (Window, Bool) ~ WindowMaximizeE c => Proxy c -> (Window, Bool) -> IO (WindowMaximizeE c)
+  windowMaximizeConv     _ = return
 #endif
-  keyConv                :: Proxy c -> KeyE                Def -> IO (KeyE                c)
-  charConv               :: Proxy c -> CharE               Def -> IO (CharE               c)
-  charModsConv           :: Proxy c -> CharModsE           Def -> IO (CharModsE           c)
-  mouseButtonConv        :: Proxy c -> MouseButtonE        Def -> IO (MouseButtonE        c)
-  cursorPosConv          :: Proxy c -> CursorPosE          Def -> IO (CursorPosE          c)
-  cursorEnterConv        :: Proxy c -> CursorEnterE        Def -> IO (CursorEnterE        c)
-  scrollConv             :: Proxy c -> ScrollE             Def -> IO (ScrollE             c)
-  dropConv               :: Proxy c -> DropE               Def -> IO (DropE               c)
+  keyConv :: Proxy c -> (Window, Key, Int, KeyState, ModifierKeys) -> IO (KeyE c)
+  default keyConv
+            :: (Window, Key, Int, KeyState, ModifierKeys) ~ KeyE c
+            => Proxy c -> (Window, Key, Int, KeyState, ModifierKeys) -> IO (KeyE c)
+  keyConv                _ = return
+
+  charConv :: Proxy c -> (Window, Char) -> IO (CharE               c)
+  default charConv :: (Window, Char) ~ CharE c => Proxy c -> (Window, Char) -> IO (CharE c)
+  charConv               _ = return
+
+  charModsConv :: Proxy c -> (Window, Char, ModifierKeys) -> IO (CharModsE c)
+  default charModsConv
+            :: (Window, Char, ModifierKeys) ~ CharModsE c => Proxy c -> (Window, Char, ModifierKeys) -> IO (CharModsE c)
+  charModsConv           _ = return
+
+  mouseButtonConv :: Proxy c -> (Window, MouseButton, MouseButtonState, ModifierKeys) -> IO (MouseButtonE c)
+  default mouseButtonConv
+            :: (Window, MouseButton, MouseButtonState, ModifierKeys) ~ MouseButtonE c
+            => Proxy c -> (Window, MouseButton, MouseButtonState, ModifierKeys) -> IO (MouseButtonE c)
+  mouseButtonConv        _ = return
+
+  cursorPosConv :: Proxy c -> (Window, Double, Double) -> IO (CursorPosE c)
+  default cursorPosConv
+            :: (Window, Double, Double) ~ CursorPosE c => Proxy c -> (Window, Double, Double) -> IO (CursorPosE c)
+  cursorPosConv          _ = return
+
+  cursorEnterConv :: Proxy c -> (Window, CursorState) -> IO (CursorEnterE c)
+  default cursorEnterConv
+            :: (Window, CursorState) ~ CursorEnterE c => Proxy c -> (Window, CursorState) -> IO (CursorEnterE c)
+  cursorEnterConv        _ = return
+
+  scrollConv :: Proxy c -> (Window, Double, Double) -> IO (ScrollE c)
+  default scrollConv
+            :: (Window, Double, Double) ~ ScrollE c => Proxy c -> (Window, Double, Double) -> IO (ScrollE c)
+  scrollConv             _ = return
+
+  dropConv :: Proxy c -> (Window, [FilePath]) -> IO (DropE c)
+  default dropConv :: (Window, [FilePath]) ~ DropE c => Proxy c -> (Window, [FilePath]) -> IO (DropE c)
+  dropConv               _ = return
 
 
 
@@ -107,45 +204,6 @@ class Conversion c where
 data Def
 
 instance Conversion Def where
-  type WindowPosE          Def = (Window, Int, Int)
-  type WindowSizeE         Def = (Window, Int, Int)
-  type WindowCloseE        Def =  Window
-  type WindowRefreshE      Def =  Window
-  type WindowFocusE        Def = (Window, Bool)
-  type WindowIconifyE      Def = (Window, Bool)
-  type FramebufferSizeE    Def = (Window, Int, Int)
-#if MIN_VERSION_GLFW_b(3,3,0)
-  type WindowContentScaleE Def = (Window, Float, Float)
-  type WindowMaximizeE     Def = (Window, Bool)
-#endif
-  type KeyE                Def = (Window, Key, Int, KeyState, ModifierKeys)
-  type CharE               Def = (Window, Char)
-  type CharModsE           Def = (Window, Char, ModifierKeys)
-  type MouseButtonE        Def = (Window, MouseButton, MouseButtonState, ModifierKeys)
-  type CursorPosE          Def = (Window, Double, Double)
-  type CursorEnterE        Def = (Window, CursorState)
-  type ScrollE             Def = (Window, Double, Double)
-  type DropE               Def = (Window, [FilePath])
-
-  windowPosConv          _ = return
-  windowSizeConv         _ = return
-  windowCloseConv        _ = return
-  windowRefreshConv      _ = return
-  windowFocusConv        _ = return
-  windowIconifyConv      _ = return
-  framebufferSizeConv    _ = return
-#if MIN_VERSION_GLFW_b(3,3,0)
-  windowContentScaleConv _ = return
-  windowMaximizeConv     _ = return
-#endif
-  keyConv                _ = return
-  charConv               _ = return
-  charModsConv           _ = return
-  mouseButtonConv        _ = return
-  cursorPosConv          _ = return
-  cursorEnterConv        _ = return
-  scrollConv             _ = return
-  dropConv               _ = return
 
 
 
